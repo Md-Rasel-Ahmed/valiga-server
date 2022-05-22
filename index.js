2c97e692bfeb826a7808149fb51e1e3014772d22
Rasel-ahemd <nhd305812@gmail.com>
2022-05-22 18:58:53 +0500

    api create for all user stored

--- index.js
+++ index.js
@@ -21,12 +21,28 @@ async function run() {
     const allToolsCollection = client
       .db("valigaHardware")
       .collection("allTools");
-
+    const usersCollection = client.db("valigaHardware").collection("user");
     //   get the all data
     app.get("/", async (req, res) => {
       const query = {};
       const cursor = allToolsCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // user added or replace on the database
+    app.put("/user", async (req, res) => {
+      let filter = { name: req.body.email };
+      let data = req.body;
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: data,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
       res.send(result);
     });
   } finally {
