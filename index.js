ef04e607196f2ab018b83a72ede77dc82ea63e84
Rasel-ahemd <nhd305812@gmail.com>
2022-05-25 20:44:08 +0500

    all finised

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -8,6 +9,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Verifining jwt token
+const verifingToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unAuthorize" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.USER_JWT_TOKEN, (err, decoded) => {
+    if (err) return res.status(403).send({ message: "forbidden" });
+    req.decoded = decoded;
+    next();
+  });
+};
+// Create a get api
+app.get("/", (req, res) => {
+  res.send("Api done");
+});
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.6plls.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -48,14 +66,23 @@ async function run() {
       }
     });
     //get all user
-    app.get("/user", async (req, res) => {
+    app.get("/user", verifingToken, async (req, res) => {
       let query = {};
       const cursor = usersCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    // make admin api request
+    // jwt auth
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const accessToken = jwt.sign(user, process.env.USER_JWT_TOKEN, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
+
     // user role change api
     app.put("/user/admin/:email", async (req, res) => {
       let email = req.params.email;
